1062cea65b0e33f090efb8f974774ebc13bc66a2
pratap0101 <pratap.medisetty@gsspec.com>
2024-07-31 08:15:56 +0530

    config-ui

--- downloader_ui/vite.config.ts
+++ downloader_ui/vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // vite.config.js
   plugins: [react()],
+  // vite.config.js
+  server: {
+    host: '0.0.0.0', // or your specific IP address
+    port: 3000 // or your desired port
+  }
 })
